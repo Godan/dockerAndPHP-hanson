7860611703a1afd793a87933b93edb6168f3c0a8
godan <matsushita@godan.xyz>
2017-10-02 21:29:15 +0900

    [modify] DB設定周り

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,5 +1,18 @@
-CREATE TABLE `ymic`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `userName` VARCHAR(256) NOT NULL,
-  `passwd` VARCHAR(512) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(256) NOT NULL,
+  `passwd` varchar(512) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userName_UNIQUE` (`userName`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `main` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(512) NOT NULL,
+  `text` varchar(1028) NOT NULL,
+  `favalit` int(11) DEFAULT '0',
+  `status` int(11) DEFAULT '0',
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
